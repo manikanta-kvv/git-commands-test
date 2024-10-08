326eb6f8640e1f2888ba26118684ef35b4aa15d2
manikanta-kvv <manikantakvv6@gmail.com>
2024-10-08 14:54:01 +0530

    test to git username

--- JavaSampleTest.java
+++ JavaSampleTest.java
@@ -2,5 +2,6 @@ public class JavaSampleTest {
 
     public static void main(String[] args) {
         System.out.println("this is the sample test case class");
+        System.out.println("this is to test the git user name");
     }
 }
