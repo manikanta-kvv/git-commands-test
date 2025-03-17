62d14a64b1ca322e9f2a31b3178bac1b51191512
manikanta-kvv <manikantakvv6@gmail.com>
2025-03-17 22:51:27 +0530

    Revert "new line added for test"
    This reverts commit 1a6544f90053689069955ddb337730bbb0be0d09.

--- JavaSampleTest.java
+++ JavaSampleTest.java
@@ -3,6 +3,5 @@ public class JavaSampleTest {
     public static void main(String[] args) {
         System.out.println("this is the sample test case class");
         System.out.println("this is to test the git user name");
-        System.out.println("this is from test branch");
     }
 }
